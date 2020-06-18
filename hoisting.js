9de9b8e39a6876b63e44fcb26edcc548d1031b8d
Samara Grant <samara.amanda01@gmail.com>
2020-06-18 01:07:42 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -20,11 +20,11 @@ thisIsCrazy();
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Kristin";
+
 
   function sayMy() {
     console.log(name);
-    var name = "Cricky";
+    var name = "Kristin";
 
   }
 sayMy();
